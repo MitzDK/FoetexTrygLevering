123ce2c6ddb71a99290e10db7e62dd0392003c3c
MitzDK <victorwilliamjejlskov@gmail.com>
2021-12-06 12:51:59 +0100

    enmasse =)

--- FoetexTrygLevering/Pages/Users/Customer/Homepage.cshtml.cs
+++ FoetexTrygLevering/Pages/Users/Customer/Homepage.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoetexTrygLevering.Models.Users;
+using FoetexTrygLevering.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace FoetexTrygLevering.Pages.Users.Customer
 {
     public class HomepageModel : PageModel
     {
-        public void OnGet()
+        public Models.Users.Customer Customer { get; set; }
+        public List<User> Users { get; set; }
+        private UserService _userService;
+
+        public HomepageModel(UserService userService)
         {
+            _userService = userService;
+        }
+        public IActionResult OnGet()
+        {
+
+            //foreach(User user in )
+            return Page();
         }
     }
 }
